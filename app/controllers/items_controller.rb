90be0ae0e1a7dd1a5f20623bcc364c3acbf34d32
Scott Donnan <scott.donnan@me.com>
2021-11-04 11:35:29 -0500

    commit tree 54c2e333626e5b3c8cd36cdaac3ebbdb4e6cbcc2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,42 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        render json: user.items
+      else
+        render json: { error: "User not found"}, status: :not_found
+      end
+    else
+      items = Item.all
+      render json: items, include: :user
+    end   
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    if item
+      render json: item
+    else
+      render json: {error: "Item not found"}, status: :not_found
+    end
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    if user
+      new_item = Item.create(item_params)
+      render json: new_item, status: :created
+    else
+      render json: {error: "User not found"}, status: :not_found
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit :name, :description, :price, :user_id
   end
 
 end
